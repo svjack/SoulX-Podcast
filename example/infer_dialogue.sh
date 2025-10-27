ae27aa6583225175c165a6bcd2bd1060607c5543
xinshengwang <846379560@qq.com>
2025-10-28 03:13:09 +0800

    add example

---
+++ example/infer_dialogue.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+echo "PYTHONPATH set to: $PYTHONPATH"
+
+# Note: To infer Chinese dialects, set model_dir to "pretrained_models/SoulX-Podcast-1.7B-dialect
+model_dir=pretrained_models/SoulX-Podcast-1.7B
+input_file=example/podcast_script/script_mandarin.json
+
+python cli/podcast.py \
+        --json_path ${input_file} \
+        --model_path ${model_dir} \
+        --output_path outputs/mandarin.wav
